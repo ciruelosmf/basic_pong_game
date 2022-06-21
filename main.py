dc33617e79e964f0ed0a959d2190642ca5b323c8
matías federico ciruelos <62398984+ciruelosmf@users.noreply.github.com>
2022-06-21 01:21:10 +0000

    paddles move

--- main.py
+++ main.py
@@ -42,8 +42,15 @@ class Paddle():
   def draw(self,win):
     pygame.draw.rect(win, self.COLOR, (self.x, self.y, self.width, self.height))
 
-  def move(self):
-
+  def move(self, up = True):
+    
+    if up:
+      self.y -= self.VEL
+    else:
+      self.y += self.VEL      
+            
+            
+  
 
 
 
@@ -55,22 +62,41 @@ class Ball():
 
 
 
+def handle_paddle_movement(keys, left_paddle, right_paddle):
+  if keys[pygame.K_w]:
+    left_paddle.move(up=True)
+  if keys[pygame.K_s]:
+    left_paddle.move(up=False)
+  if keys[pygame.K_UP]:
+    right_paddle.move(up=True)
+  if keys[pygame.K_DOWN]:
+    right_paddle.move(up=False)
+    
+
+
+
     
 def main():
 
   right_paddle = Paddle(WIDTH - 10 - padd_width, HEIGHT//2 - padd_height // 2,padd_width,padd_height )
+  
   left_paddle = Paddle(10, HEIGHT//2 - padd_height // 2,padd_width,padd_height )
+  
   run = True
   clock = pygame.time.Clock()
   
   while run:
     clock.tick(FPS)
     draw(win, [left_paddle, right_paddle])
+    
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         run = False
         break
-        
+  #
+  
+    keys = pygame.key.get_pressed()
+    handle_paddle_movement(keys, left_paddle, right_paddle)
   pygame.quit()
 
 
